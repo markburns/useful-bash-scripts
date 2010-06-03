e59cc33f2863eb9a07840d557383e49dd4e0671a
Mark Burns <mark@mark.rawnet.local>
2010-06-03 16:57:25 +0100

    altered lsm

--- lsm.rb
+++ lsm.rb
@@ -11,7 +11,7 @@ include Term::ANSIColor
 
 version = `rake db:version`
 sort_alphabetically = ARGV[0] == "s"
-
+show_filename = ARGV.index "-f"
 time = version.split(":")[1]
 current_version = time.chomp!.strip!
 time = DateTime.parse(time).to_s
@@ -26,10 +26,11 @@ list = `ls db/migrate`
 arr = []
 longest=0
 list.each do |i|
+  filename = i
   name= i[15..-5]
   time=i[0..13]
   longest = name.length if name.length > longest
-  arr << [name,time]
+  arr << [name,time, filename]
 end
 
 if sort_alphabetically
@@ -39,7 +40,7 @@ if sort_alphabetically
 end
 
 arr.each do |details|
-  name,time = details[0], details[1]
+  name,time, filename = details[0], details[1], details[2]
   name.gsub! "_"," "
   spaces=(longest - name.length)+5
   colorize = true if time==current_version
@@ -49,8 +50,12 @@ arr.each do |details|
   time = time.split("T")[1][0..7]
 
   puts red if colorize
-  puts name + (" "*spaces) + "| " + date + " " + time
+  display = name + (" "*spaces) + "| " + date + " " + time
+  if show_filename
+    display << ("   " + blue + filename + white )
+  end
 
+  puts display
   #reset colors
   if colorize
     colorize = false
